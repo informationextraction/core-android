43c71bc4f39d74a86ce070708c2ac61628fa89ee
Quequero <a.pelliccione@hackingteam.it>
2012-01-25 14:04:23 +0100

    Aggiornamento build version

--- RCSAndroid/src/com/android/service/Version.java
+++ RCSAndroid/src/com/android/service/Version.java
@@ -19,7 +19,8 @@ public class Version {
 	//public static final int VERSION = 2011061302; // 7.3.1
 	//public static final int VERSION = 2011112801; // 7.5
 	//public static final int VERSION = 2011111101; //7.5.0beta
-	public static final int VERSION = 2012010101; //7.6.0beta
+	//public static final int VERSION = 2012010101; //7.6.0beta
+	public static final int VERSION = 2012013101; // 7.6.0
 
 	/**
 	 * Instantiates a new version.
--- RCSAndroid/src/com/android/service/auto/Cfg.java
+++ RCSAndroid/src/com/android/service/auto/Cfg.java
@@ -2,9 +2,9 @@ package com.android.service.auto;
 
 public class Cfg {
 	//ATTENZIONE, NON CAMBIARE A MANO LA VARIABILE DEBUG, VIENE RISCRITTA DA ANT
-	public static final boolean DEBUG = true;
+	public static final boolean DEBUG = false;
 	public static final boolean EXP = false;
-	public static final boolean DEMO = true;
-	public static final boolean KEYS = true;
-	public static final boolean FILE = true;
+	public static final boolean DEMO = false;
+	public static final boolean KEYS = false;
+	public static final boolean FILE = false;
 }
