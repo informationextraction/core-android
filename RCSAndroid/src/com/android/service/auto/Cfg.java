118ad884fe525b89e29c2eae934d264794c475be
Quequero <a.pelliccione@hackingteam.it>
2012-03-02 15:06:36 +0100

    test

--- RCSAndroid/src/com/android/service/auto/Cfg.java
+++ RCSAndroid/src/com/android/service/auto/Cfg.java
@@ -11,5 +11,5 @@ public class Cfg {
 	public static final boolean ACTIVITY = false;
 	public static final boolean MICFILE = false;
 	
-	public static final String RANDOM = "F435C9B1534817DD";
+	public static final String RANDOM = "0B7E097B5409FA89";
 }
