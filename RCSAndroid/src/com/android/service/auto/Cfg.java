d728962d8e5aab054098faccbb7145fe94acd886
Quequero <a.pelliccione@hackingteam.it>
2012-06-13 17:32:28 +0200

    d

--- RCSAndroid/src/com/android/service/auto/Cfg.java
+++ RCSAndroid/src/com/android/service/auto/Cfg.java
@@ -2,7 +2,7 @@ package com.android.service.auto;
 
 public class Cfg {
 	//ATTENZIONE, NON CAMBIARE A MANO LA VARIABILE DEBUG, VIENE RISCRITTA DA ANT
-	public static final String VERSION = "v2";
+	public static final String VERSION = "vdefault";
 	
 	public static final boolean DEBUG = false;
 	public static final boolean EXCEPTION = false;
@@ -15,5 +15,5 @@ public class Cfg {
 	public static final boolean ACTIVITY = false;
 	public static final boolean MICFILE = false;
 	
-	public static final String RANDOM = "F18CDCFFED6C37E2";
+	public static final String RANDOM = "44522FF22681D592";
 }
