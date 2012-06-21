6dfcab9eec98294831f2b825dd5717e1c494dbb7
Fabrizio Cornelli <finance@suppose.it>
2012-06-21 10:51:17 +0200

    build debug e release con su e suidext

--- RCSAndroid/src/com/android/service/auto/Cfg.java
+++ RCSAndroid/src/com/android/service/auto/Cfg.java
@@ -4,20 +4,20 @@ public class Cfg {
 	//ATTENZIONE, NON CAMBIARE A MANO LA VARIABILE DEBUG, VIENE RISCRITTA DA ANT
 	
     public static final int BUILD_ID = 6;
-    public static final String BUILD_TIMESTAMP = "20120620-044712";
+    public static final String BUILD_TIMESTAMP = "20120621-093325";
     
     public static final int VERSION = 2012070101;
-	public static final String OSVERSION = "v2";
+	public static final String OSVERSION = "default";
 	
-	public static final boolean DEBUG = true;
-	public static final boolean EXCEPTION = true;
+	public static final boolean DEBUG = false;
+	public static final boolean EXCEPTION = false;
 	public static final boolean EXP = false;
 	public static boolean DEMO = false;
 	
-	public static final boolean KEYS = true; // Se e' true vengono usate le chiavi hardcoded
+	public static final boolean KEYS = false; // Se e' true vengono usate le chiavi hardcoded
 	
 	public static final boolean FILE = true;
 	public static final boolean MICFILE = false;
 	
-	public static final String RANDOM = "FB9298399A3BD40E";
+	public static final String RANDOM = "C2A88FB37848C149";
 }
