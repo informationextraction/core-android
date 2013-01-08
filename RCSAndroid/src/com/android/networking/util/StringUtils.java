df08deee558ec516da7f905a8a38e7f5f1e3fa10
Zeno <zeno@hackingteam.it>
2013-01-08 17:09:08 +0100

    bug isempty
    Signed-off-by: Zeno <zeno@hackingteam.it>

--- RCSAndroid/src/com/android/networking/util/StringUtils.java
+++ RCSAndroid/src/com/android/networking/util/StringUtils.java
@@ -244,6 +244,6 @@ public class StringUtils {
 	}
 
 	public static boolean isEmpty(String string) {
-		return string!=null && string.length() > 0;
+		return string==null || string.length() == 0;
 	}
 }
