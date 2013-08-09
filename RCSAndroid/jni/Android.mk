5ff56a3f7c073acb700b351ab4f26049abc0d262
Quequero <a.pelliccione@hackingteam.it>
2013-08-09 14:46:47 +0200

    Offuscati gli exploit, ora si va in vacanza pero'!

--- RCSAndroid/jni/Android.mk
+++ RCSAndroid/jni/Android.mk
@@ -19,15 +19,19 @@ include $(CLEAR_VARS)
 LOCAL_MODULE    := suidext
 LOCAL_SRC_FILES := suidext.c
 
+include $(BUILD_EXECUTABLE)
+
 include $(CLEAR_VARS)
 
-LOCAL_MODULE    := ec
-LOCAL_SRC_FILES := ec.c
+LOCAL_MODULE    := expl_check
+LOCAL_SRC_FILES := expl_check.c
+
+include $(BUILD_EXECUTABLE)
 
 include $(CLEAR_VARS)
 
 LOCAL_MODULE    := local_exploit
-LOCAL_SRC_FILES :=  local_exploit.c exploit_list.c kallsyms_in_memory.c kallsyms.c
+LOCAL_SRC_FILES := local_exploit.c exploit_list.c kallsyms_in_memory.c kallsyms.c
 LOCAL_C_INCLUDES := headers
 
 include $(BUILD_EXECUTABLE)
